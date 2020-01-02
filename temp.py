0ba24bb30da6eeb7f762c6a5a7d47cc16297f5e9
zhenclick750 <zhen.li@cookmedical.com>
2020-01-02 16:11:00 -0500

    define a function

--- temp.py
+++ temp.py
@@ -5,3 +5,17 @@ Spyder Editor
 This is a temporary script file.
 """
 print("hello world")
+
+# Import hello module
+import hello
+
+
+# call function
+hello.world()
+
+# print variable
+print(hello.shark)
+
+# Call class
+jesse = hello.Octopus("Jesse", "orange")
+jesse.tell_me_about_the_octopus()
